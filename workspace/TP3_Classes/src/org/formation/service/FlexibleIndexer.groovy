8029ae3ae0bed3d4095a2d18192cdbf7b9ace1a4
David THIBAU <david.thibau@windows.com>
2020-04-20 15:50:42 +0200

    Solution TP3

---
+++ workspace/TP3_Classes/src/org/formation/service/FlexibleIndexer.groovy
@@ -0,0 +1,34 @@
+package org.formation.service
+
+import org.formation.model.Index
+
+class FlexibleIndexer {
+	String tokenizer
+	int minimalSize =2
+
+	
+
+	private def fillMap = {
+		int occurence = map.get(it.toLowerCase(),0);
+		map[it.toLowerCase()]=occurence+1;
+	}
+
+	public def doIndex(def index) {
+		println 'Indexation with ' + minimalSize + " " + tokenizer
+		String texte = index.source.toString()
+		index.map = _index(texte)
+		index.indexed = new Date()
+		index
+	}
+
+	private Map _index(String texte) {
+		def words = texte.split(/\${tokenizer}/)
+		def map = [:]
+
+		words.findAll({it.size()>minimalSize}).each({
+			int occurence = map.get(it.toLowerCase(),0);
+			map[it.toLowerCase()]=occurence+1;
+		})
+		return map
+	}
+}
